f5b3cc979ddf6bb50a571e4ad5e3e791594464ab
lpg2709 <leonardo_guarezi@hotmail.com>
2020-10-26 21:35:59 -0300

    Add opcodes0x0000 to 0x5

--- src/4004_chip.c
+++ src/4004_chip.c
@@ -1,4 +1,5 @@
 #include "./4004_chip.h"
+#include <bits/stdint-uintn.h>
 #include <stdio.h>
 #include <string.h>
 #include <strings.h>
@@ -10,6 +11,9 @@ void init4004(chip_4004 *c){
 	memset((void*) c->STACK.addrs, 0, 3);
 	c->STACK.SP = 0;
 	c->PC = 0;
+	c->ACC = 0;
+	c->carry = false;
+	c->test = false;
 }
 
 bool loadProgramROM(chip_4004 *c, const char *program){
@@ -20,4 +24,65 @@ bool loadProgramROM(chip_4004 *c, const char *program){
 	}
 	memcpy(c->ROM, program, program_size);
 	return true;
+}
+
+void chip_cycle(chip_4004 *c, uint32_t cycles_limit){
+	uint32_t cycle = 0;
+
+	while(cycle < cycles_limit){
+
+
+	}
+}
+
+void opcode_nop(){
+	return;
+}
+
+void opcode_jcn(chip_4004 *c, uint16_t opa){
+	uint8_t C1 = (opa & 0x800) >> 11;
+	uint8_t C2 = (opa & 0x400) >> 10;
+	uint8_t C3 = (opa & 0x200) >> 9;
+	uint8_t C4 = (opa & 0x100) >> 8;
+	uint8_t addr = (opa & 0xFF) | (0xF << 8);
+
+	uint8_t acc_is_0 = (c->ACC == 0);
+	
+	if((!C1 && ((acc_is_0 && C2) || (c->carry && C3) || (!c->test && C4))) || (C1 && ((acc_is_0 || !C2) && (c->carry || C3) && (c->test || !C4))))
+		c->PC &= addr;
+	else
+		c->PC += 2;
+}
+
+void opcode_fim(chip_4004 *c, uint16_t opa){
+	uint8_t IR_pair = (opa & 0xE00) >> 8;	
+	uint8_t rom_data = (opa & 0xFF);
+
+	c->IR[IR_pair] = (rom_data & 0xF0)>>4;
+	c->IR[IR_pair+1] = (rom_data & 0xF);
+
+	c->PC += 2;
+}
+
+void opcode_fin(chip_4004 *c, uint8_t opa){
+	uint8_t IR_pair = (opa & 0xE);	
+	uint16_t rom_addrs = (((c->PC + 1) % ROM_PAGE_SIZE == 1) ? ((c->PC+1) & 0xF00): (c->PC & 0xF00)) | (c->IR[0] << 4) | (c->IR[1]);
+	c->IR[IR_pair] = (c->ROM[rom_addrs] & 0xF);
+	c->IR[IR_pair+1] = (c->IR[rom_addrs] & 0xF0) >> 4;
+	c->PC++;
+}
+
+void opcode_jin(chip_4004 *c, uint8_t opa){
+	uint8_t IR_pair = (opa & 0xE);	
+	c->PC  = (((c->PC + 1) % ROM_PAGE_SIZE == 1) ? ((c->PC+1) & 0xF00): (c->PC & 0xF00)) | (c->IR[IR_pair] << 4) | (c->IR[IR_pair + 1]);
+}
+
+void opcode_jun(chip_4004 *c, uint16_t opa){
+	c->PC = (opa & 0XFFF);
+}
+
+void opcode_jms(chip_4004 *c, uint16_t opa){
+	c->STACK.addrs[c->STACK.SP] = c->PC;
+	c->STACK.SP == 2 ? c->STACK.SP = 0: c->STACK.SP++;	
+	c->PC = (opa & 0XFFF);
 }
--- src/4004_chip.h
+++ src/4004_chip.h
@@ -1,13 +1,15 @@
 #ifndef CHIP4004_H
 #define CHIP4004_H
 
+#include <bits/stdint-uintn.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-#define ROM_PAGE_SIZE 255
+#define ROM_PAGE_SIZE 256
+#define WORD_I 8
 
 typedef struct sct_chip_4004 chip_4004;
 
@@ -20,10 +22,28 @@ struct sct_chip_4004{
 		uint8_t SP:2;             // Stack Pointer
 	}STACK;
 	uint16_t PC;                  // on 4004 are 12 bits not 16
+	uint8_t ACC;
+	bool carry;
+	bool test;
 };
 
 void init4004(chip_4004 *c);
 bool loadProgramROM(chip_4004 *c, const char *program);
 
+void chip_cycle(chip_4004 *c, uint32_t cycles_limit);
+void chip_reset(chip_4004 *c); 		// Reset all chip
+
+
+/*
+ * Instruction Set
+ * */
+
+void opcode_nop(); 								// 0x0000
+void opcode_jcn(chip_4004 *c, uint16_t opa); 	// 0x1---
+void opcode_fim(chip_4004 *c, uint16_t opa); 	// 0x2---
+void opcode_fin(chip_4004 *c, uint8_t opa);  	// 0x3-0
+void opcode_jin(chip_4004 *c, uint8_t opa); 	// 0x3-1
+void opcode_jun(chip_4004 *c, uint16_t opa); 	// 0x4---
+void opcode_jms(chip_4004 *c, uint16_t opa); 	// 0x5---
 
 #endif
