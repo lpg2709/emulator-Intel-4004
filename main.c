a4ccd3f35552c8c7e26ec0d484eb6e032a6a055c
lpg2709 <leonardo_guarezi@hotmail.com>
2022-01-18 20:29:30 -0300

    I don't remmember

--- main.c
+++ main.c
@@ -2,30 +2,29 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
 #include "./src/4004_chip.h"
 #include "./src/files.h"
 #include "./src/gui_cli.h"
 #include "./src/application.h"
+#include "./src/options_parser.h"
 
-int main(){
-	
-	long size;
-	uint8_t *rom_ram_test = (uint8_t*) b_read_file("./tests_src/WRM", &size);
+int main(int argc, char **argv){
+	Options options;
+	options_parser(argc, argv, &options);
+
 	chip_4004 myChip;
 	init4004(&myChip);
 
-	loadProgramROM(&myChip, rom_ram_test, size);
-
-	application_init();
+	application_init(options, &myChip);
 
 	application_main_loop(&myChip);
+	// cli_d_print_rom_memory(&myChip);
 
-	
-	cli_d_print_rom_memory(&myChip);
-	
 	/*
-	uint8_t hex[] = {0x20, 0xB4, 0x21, 0xDF, 0xE0};
-	b_write_file("./tests_src/WRM", hex, sizeof(*hex), sizeof(hex));
+	// Write data in binary for test
+	uint8_t hex[] = { 0x20, 0x00, 0x22, 0x00, 0xDC, 0xB2, 0x21, 0xE4, 0xF2, 0xE5, 0xF2, 0xE6, 0xF2, 0xE7, 0xF2, 0x60, 0x72, 0x06 };
+	b_write_file("./roms/bin/ram_status_test", hex, sizeof(*hex), sizeof(hex));
 	*/
 	return 0;
 }
