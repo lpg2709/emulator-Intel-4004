c74e465c74b4bc1ae1aaf8f2283af92e43f49de4
Leonardo Padilha Guarezi <leonardo_guarezi@hotmail.com>
2024-05-15 11:15:15 -0300

    ADD more errors and operands information

--- src/opcode.h
+++ src/opcode.h
@@ -3,20 +3,20 @@
 
 // OPCODE, VALUE, String name, operands
 #define X_OPCODE X(NOP, 0x00, "NOP", 0) \
-	X(JCN, 0x01, "JCN", 0) \
-	X(FIM, 0x02, "FIM", 0) \
-	X(FIN, 0x03, "JIN", 0) \
-	X(JIN, 0x03, "JUN", 0) \
-	X(JUN, 0x05, "JUN", 0) \
-	X(JMS, 0x06, "JMS", 0) \
-	X(INC, 0x05, "INC", 0) \
-	X(ISZ, 0x07, "ISZ", 0) \
-	X(ADD, 0x08, "ADD", 0) \
-	X(SUB, 0x09, "SUB", 0) \
-	X(LD , 0x0A, "LD" , 0) \
-	X(XCH, 0x0B, "XCH", 0) \
-	X(BBL, 0x0C, "BBL", 0) \
-	X(LDM, 0x0D, "LDM", 0) \
+	X(JCN, 0x01, "JCN", 2) \
+	X(FIM, 0x02, "FIM", 2) \
+	X(FIN, 0x03, "JIN", 1) \
+	X(JIN, 0x03, "JUN", 1) \
+	X(JUN, 0x05, "JUN", 2) \
+	X(JMS, 0x06, "JMS", 2) \
+	X(INC, 0x05, "INC", 1) \
+	X(ISZ, 0x07, "ISZ", 2) \
+	X(ADD, 0x08, "ADD", 1) \
+	X(SUB, 0x09, "SUB", 1) \
+	X(LD , 0x0A, "LD" , 1) \
+	X(XCH, 0x0B, "XCH", 1) \
+	X(BBL, 0x0C, "BBL", 1) \
+	X(LDM, 0x0D, "LDM", 1) \
 	X(CLB, 0xF0, "CLB", 0) \
 	X(CLC, 0xF1, "CLC", 0) \
 	X(IAC, 0xF2, "IAC", 0) \
@@ -30,7 +30,7 @@
 	X(DAA, 0xFB, "DAA", 0) \
 	X(KBP, 0xFC, "KBP", 0) \
 	X(DCL, 0xFD, "DCL", 0) \
-	X(SRC, 0x20, "SRC", 0) \
+	X(SRC, 0x20, "SRC", 1) \
 	X(WRM, 0xE0, "WRM", 0) \
 	X(WMP, 0xE1, "WMP", 0) \
 	X(WRR, 0xE2, "WRR", 0) \
